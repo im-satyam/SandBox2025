2f0890521936fca4ed5a0df50f4064bab205d7ac
adios-xo <adityaontech@gmail.com>
2025-03-01 14:18:53 +0530

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,14 +2,20 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <nav className="flex justify-between items-center bg-gray-800 text-white p-4 shadow-md">
+        <nav className="flex justify-between items-center bg-black text-[#6cff33] p-4 shadow-md">
             <div className="text-xl font-bold">YourSite</div>
-            <div>
+            <div className="flex space-x-6">
                 <ul className="flex space-x-6">
                     <li className="hover:text-gray-400 cursor-pointer">About</li>
                     <li className="hover:text-gray-400 cursor-pointer">Contact</li>
                     <li className="hover:text-gray-400 cursor-pointer">Socials</li>
                 </ul>
+                <button className="p-2.5 border-2 border-[#6cff33] rounded-full">
+                    Login
+                </button>
+                <button className="p-2.5 border-2 border-[#6cff33] rounded-full">
+                    Sign Up
+                </button>
             </div>
         </nav>
 
